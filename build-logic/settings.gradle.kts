fff08c2daf8ac1707c3d314475c0e1c060da5e51
maximilianpilz <maximilian.pilz.git@gmail.com>
2025-04-07 15:03:29 +0200

    partial cleanup

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -9,8 +9,3 @@ dependencyResolutionManagement {
         }
     }
 }
-
-
-include("versions-platform")
-
-project(":versions-platform").projectDir = file("../versions-platform")
--- sdccc/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-include("versions-platform")
-
-project(":versions-platform").projectDir = file("../versions-platform")
